cfa52cb24d89ff862deabf16cea203312bd6aeeb
Philippe Turner <philippeturner@gmail.com>
2019-12-13 11:10:57 -0500

    [wip] - Add colors

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ import copy
 import json
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RED = "\033[1;31m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+    REVERSE = "\033[;7m"
+
+
 def find_ressource_index(ressources, value_to_find):
     for i, ressource in enumerate(ressources):
         if ressource['name'] == value_to_find:
@@ -20,19 +37,27 @@ def main():
     ressource_to_replace = os.getenv('REPLACED_RESSOURCE')
     out_folder = os.getenv('OUT_FOLDER')
     pipeline_file = os.getenv('PIPELINE_FILENAME')
-    print('Opening the file {} to replace the ressource : {}'.format(
-        pipeline_file, ressource_to_replace))
+    print(bcolors.HEADER + 'Opening the file {} to replace the ressource : {}'.format(
+        pipeline_file, ressource_to_replace) + bcolors.ENDC)
     with open(pipeline_file, 'r') as f:
         template_yml = yaml.load(f, Loader=yaml.FullLoader)
     print('Gathering branch info from repository')
     res = requests.get(
         "https://api.github.com/repos/{}/{}/branches".format(project, repo))
+    if(res.status_code != 200):
+        print(bcolors.FAIL + 'Error with branches gathering request' + bcolors.ENDC)
+        sys.exit(1)
     j = res.json()
     ressource_i = find_ressource_index(
         template_yml['resources'], ressource_to_replace)
+    if(ressource_i == -1):
+        print(bcolors.FAIL +
+              'Ressource that needs to be changed doesn\'t exist' + bcolors.ENDC)
+        sys.exit(1)
     for branch_info in j:
         branch_name = branch_info['name']
-        print('Creating job for branch name : {}'.format(branch_name))
+        print(bcolors.BLUE +
+              'Creating job for branch name : {}'.format(branch_name) + bcolors.ENDC)
         new_ressource = copy.deepcopy(template_yml['resources'][ressource_i])
         new_ressource['source']['branch'] = branch_name
         new_ressource['name'] = 'git-' + branch_name
@@ -49,15 +74,15 @@ def main():
             new_yaml['jobs'].append(new_job)
             new_group['jobs'].append(new_job['name'])
         new_yaml['groups'].append(new_group)
-        print('New groups :')
-        for group in new_yaml['groups']:
-            print(' - {}'.format(group['name']))
+    print(bcolors.BLUE + 'New groups :' + bcolors.ENDC)
+    for group in new_yaml['groups']:
+        print(' - {}'.format(group['name']))
     print('New pipeline done, the pipeline will be written in {}/{}/new_pipeline.yaml'.format(os.pardir, out_folder))
     with open('{}/{}/new_pipeline.yaml'.format(os.pardir, out_folder), 'w') as f:
         noalias_dumper = yaml.dumper.SafeDumper
         noalias_dumper.ignore_aliases = lambda self, data: True
         yaml.dump(new_yaml, f, default_flow_style=False, Dumper=noalias_dumper)
-    print('Job done !!!')
+    print(bcolors.GREEN + 'Job done !!!' + bcolors.ENDC)
 
 
 if __name__ == '__main__':
